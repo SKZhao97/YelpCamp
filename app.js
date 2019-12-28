4dadeb688547fbf47c5dcecb80ef36ee57c5853f
sikai <sikaizhao@gmail.com>
2019-12-28 20:02:43 +0000

    add port option

--- app.js
+++ app.js
@@ -53,7 +53,8 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/", indexRoutes);
 
-app.listen(3000, function() {
+var port = process.env.PORT || 3000
+app.listen(port, function() {
 //app.listen(process.env.PORT, process.env.IP, function() {
 	console.log("**********The YelpCamp Server Has Started!**********");
 });
